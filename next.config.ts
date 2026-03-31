416992c12b57b2e6f625467e7ffbbe269f25ffac
Rofii29 <cibihijau12345@gmail.com>
2026-03-31 19:08:14 +0700

    fix: update image remote pattern protocol from https to http for havia.id domains

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const nextConfig: NextConfig = {
         hostname: "brain.havia.id",
       },
       {
-        protocol: "https",
+        protocol: "http",
         hostname: "*havia.id*",
       },
     ],
